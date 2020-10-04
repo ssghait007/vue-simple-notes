ed603dbf19b2ac2e297f40f29d9dd9c30366389d
1VinceP <vpalmergraphics@gmail.com>
2020-10-04 16:24:23 -0400

    create and display notes list

---
+++ src/components/Note.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+   name: 'note',
+
+   props: {
+      note: { type: Object, default: () => ({}) },
+      selected: { type: Boolean, default: false },
+   },
+};
+</script>
+
+<template>
+   <div :class="['note', { selected }]">
+      {{ note.name }}
+   </div>
+</template>
+
+<style scoped lang="scss">
+.note {
+   width: 100%;
+   height: 40px;
+   border-left: 5px solid transparent;
+   border-bottom: 1px solid #323231;
+   cursor: pointer;
+   transition: .15s background ease-in-out;
+   &:first-child { border-top: 1px solid #323231; }
+   &:hover { background: rgba(255, 255, 255, 0.08); }
+   &.selected {
+      border-left-color: #faa500;
+   }
+}
+</style>
