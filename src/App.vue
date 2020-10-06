62a45b0f2d607334b93c75f241bbc707e89ac0c1
Michał Starski <michalstarski99@gmail.com>
2020-10-06 21:47:11 +0200

    Added markdown markdown support

--- src/App.vue
+++ src/App.vue
@@ -1,130 +1,130 @@
 <script>
-import moment from 'moment';
-import Note from '@/components/Note.vue';
-import NoteEdit from '@/views/NoteEdit.vue';
+import moment from "moment";
+import Note from "@/components/Note.vue";
+import NoteEdit from "@/views/NoteEdit.vue";
 
 class NoteClass {
-   constructor() {
-      this.name = 'New Note';
-      this.content = '';
-      this.creationDate = moment(new Date()).format('MMMM Do YYYY, h:mm:ss a');
-   }
+  constructor() {
+    this.name = "New Note";
+    this.content = "";
+    this.creationDate = moment(new Date()).format("MMMM Do YYYY, h:mm:ss a");
+  }
 }
 
 export default {
-   name: 'app',
+  name: "app",
 
-   components: { Note, NoteEdit },
+  components: { Note, NoteEdit },
 
-   data: () => ({
-      notes: [],
-      search: '',
-      selected: 0,
-      saved: true,
-      navOpen: false,
-      interval: '',
-   }),
+  data: () => ({
+    notes: [],
+    search: "",
+    selected: 0,
+    saved: true,
+    navOpen: false,
+    interval: "",
+  }),
 
-   computed: {
-      filteredNotes() {
-         if (this.search) {
-            return this.notes.filter(note => {
-               return note.name.toLowerCase().includes(this.search.toLowerCase());
-            });
-         }
-
-         return this.notes;
-      },
-   },
-
-   created() {
-      const notes = JSON.parse(localStorage.getItem('simple-notes'));
-      if (notes) {
-         this.notes = notes
-      } else {
-         localStorage.setItem('simple-notes', JSON.stringify([]));
+  computed: {
+    filteredNotes() {
+      if (this.search) {
+        return this.notes.filter((note) => {
+          return note.name.toLowerCase().includes(this.search.toLowerCase());
+        });
       }
 
-      this.autoSave();
-   },
+      return this.notes;
+    },
+  },
 
-   destroyed() {
-      clearInterval(this.interval);
-   },
+  created() {
+    const notes = JSON.parse(localStorage.getItem("simple-notes"));
+    if (notes) {
+      this.notes = notes;
+    } else {
+      localStorage.setItem("simple-notes", JSON.stringify([]));
+    }
 
-   methods: {
-      autoSave() {
-         this.interval = setInterval(() => {
-            if (!this.saved) {
-               localStorage.setItem('simple-notes', JSON.stringify(this.notes));
-               this.saved = true;
-            }
-         }, 1000 * 5); // save every 5 seconds
-      },
+    this.autoSave();
+  },
 
-      newNote() {
-         this.notes.unshift(new NoteClass());
-      },
+  destroyed() {
+    clearInterval(this.interval);
+  },
 
-      selectNote(index) {
-         this.selected = index;
-         this.navOpen = false;
-      },
+  methods: {
+    autoSave() {
+      this.interval = setInterval(() => {
+        if (!this.saved) {
+          localStorage.setItem("simple-notes", JSON.stringify(this.notes));
+          this.saved = true;
+        }
+      }, 1000 * 5); // save every 5 seconds
+    },
 
-      editNote(key, e) {
-         this.notes[this.selected][key] = e.target.value;
-         this.saved = false;
-      },
+    newNote() {
+      this.notes.unshift(new NoteClass());
+    },
 
-      deleteNote(e, index) {
-         e.stopPropagation();
-         const confirmed = window.confirm('Delete note?');
-         if (confirmed) {
-            const newNotes = [...this.notes];
-            newNotes.splice(index, 1);
-            this.notes = newNotes;
-         }
-      },
-   },
+    selectNote(index) {
+      this.selected = index;
+      this.navOpen = false;
+    },
+
+    editNote(key, e) {
+      this.notes[this.selected][key] = e.target.value;
+      this.saved = false;
+    },
+
+    deleteNote(e, index) {
+      e.stopPropagation();
+      const confirmed = window.confirm("Delete note?");
+      if (confirmed) {
+        const newNotes = [...this.notes];
+        newNotes.splice(index, 1);
+        this.notes = newNotes;
+      }
+    },
+  },
 };
 </script>
 
 <template>
-   <div id="app">
-      <header class="header">
-         <div :class="['menu', { navOpen }]" @click="navOpen = !navOpen">&#10095;</div>
-         <div class="title">Simple Notes</div>
-         <div class="last-saved val">{{ saved ? 'Saved' : 'Unsaved' }}</div>
-      </header>
+  <div id="app">
+    <header class="header">
+      <div :class="['menu', { navOpen }]" @click="navOpen = !navOpen">&#10095;</div>
+      <div class="title">Simple Notes</div>
+      <div class="last-saved val">{{ saved ? 'Saved' : 'Unsaved' }}</div>
+    </header>
 
-      <div class="body">
-         <section :class="['nav', { navOpen }]">
-            <div class="search">
-               <input placeholder="Search" v-model="search" />
-               <div class="new-note" @click="newNote">+</div>
-            </div>
+    <div class="body">
+      <section :class="['nav', { navOpen }]">
+        <div class="search">
+          <input placeholder="Search" v-model="search" />
+          <div class="new-note" @click="newNote">+</div>
+        </div>
 
-            <div class="notes">
-               <Note
-                  v-for="(note, i) in filteredNotes"
-                  :key="i"
-                  :note="note"
-                  :selected="selected === i"
-                  @click="selectNote(i)"
-                  @delete="e => deleteNote(e, i)"
-               />
-            </div>
-         </section>
+        <div class="notes">
+          <Note
+            v-for="(note, i) in filteredNotes"
+            :key="i"
+            :note="note"
+            :selected="selected === i"
+            @click="selectNote(i)"
+            @delete="e => deleteNote(e, i)"
+          />
+        </div>
+      </section>
 
-         <section class="window">
-            <NoteEdit
-               :note="notes[selected]"
-               @name="e => editNote('name', e)"
-               @content="e => editNote('content', e)"
-            />
-         </section>
-      </div>
-   </div>
+      <section class="window">
+        <NoteEdit
+          :note="notes[selected]"
+          @name="e => editNote('name', e)"
+          @content="e => editNote('content', e)"
+        />
+      </section>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
@@ -133,137 +133,155 @@ $darker: #212121;
 $white: #f5f5f5;
 
 #app {
-   height: 100vh;
-   max-height: 100vh;
-   width: 100%;
-   background: $darker;
-   overflow: hidden;
-   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-   -webkit-font-smoothing: antialiased;
-   -moz-osx-font-smoothing: grayscale;
-   text-align: center;
-   color: $white;
+  height: 100vh;
+  max-height: 100vh;
+  width: 100%;
+  background: $darker;
+  overflow: hidden;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $white;
 }
 
 .header {
-   height: 40px;
-   width: 100%;
-   display: flex;
-   justify-content: space-between;
-   align-items: center;
-   padding: 0px 30px;
-   position: relative;
-   z-index: 10;
-   box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.5);
+  height: 40px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 30px;
+  position: relative;
+  z-index: 10;
+  box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.5);
 
-   div { flex: 1; }
+  div {
+    flex: 1;
+  }
 
-   .menu {
-      display: none;
-   }
+  .menu {
+    display: none;
+  }
 
-   .title {
+  .title {
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  .last-saved {
+    display: flex;
+    justify-content: flex-end;
+    color: #888;
+    font-size: 12px;
+  }
+
+  @media (max-width: 750px) {
+    .menu {
       display: flex;
       justify-content: flex-start;
-   }
-
-   .last-saved {
-      display: flex;
-      justify-content: flex-end;
-      color: #888;
-      font-size: 12px;
-   }
-
-   @media (max-width: 750px) {
-      .menu {
-         display: flex;
-         justify-content: flex-start;
-         align-items: center;
-         &.navOpen {
-            justify-content: flex-end;
-            transform: rotate(180deg);
-         }
+      align-items: center;
+      &.navOpen {
+        justify-content: flex-end;
+        transform: rotate(180deg);
       }
+    }
 
-      .title { justify-content: center; }
-   }
+    .title {
+      justify-content: center;
+    }
+  }
 }
 
 .body {
-   height: calc(100% - 40px);
-   width: 100%;
-   display: flex;
-   position: relative;
+  height: calc(100% - 40px);
+  width: 100%;
+  display: flex;
+  position: relative;
 
-   .nav {
+  .nav {
+    height: 100%;
+    width: 240px;
+    background: $darker;
+    padding-top: 10px;
+    padding-bottom: 40px;
+    position: relative;
+    z-index: 9;
+    box-shadow: 3px 0px 5px rgba(0, 0, 0, 0.25);
+
+    .search {
+      height: 26px;
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 10px;
+      align-items: center;
+
+      input {
+        height: 100%;
+        width: calc(100% - 32px);
+        background: $dark;
+        color: $white;
+        border: 1px solid $dark;
+        padding: 0px 6px;
+        outline: none;
+        &:focus {
+          border: 1px solid #888888;
+        }
+      }
+
+      .new-note {
+        height: 26px;
+        width: 32px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        transition: 0.1s background ease-in-out;
+        &:hover {
+          background: rgba(255, 255, 255, 0.25);
+        }
+        &:active {
+          background: rgba(255, 255, 255, 0.12);
+        }
+      }
+    }
+
+    .notes {
       height: 100%;
-      width: 240px;
-      background: $darker;
-      padding-top: 10px;
-      padding-bottom: 40px;
-      position: relative;
-      z-index: 9;
-      box-shadow: 3px 0px 5px rgba(0, 0, 0, 0.25);
-
-      .search {
-         height: 26px;
-         width: 100%;
-         display: flex;
-         justify-content: space-between;
-         margin-bottom: 10px;
-         align-items: center;
-
-         input {
-            height: 100%;
-            width: calc(100% - 32px);
-            background: $dark;
-            color: $white;
-            border: 1px solid $dark;
-            padding: 0px 6px;
-            outline: none;
-            &:focus { border: 1px solid #888888 }
-         }
-
-         .new-note {
-            height: 26px;
-            width: 32px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            cursor: pointer;
-            transition: .10s background ease-in-out;
-            &:hover { background: rgba(255, 255, 255, 0.25); }
-            &:active { background: rgba(255, 255, 255, 0.12); }
-         }
+      width: 100%;
+      border-top: 1px solid $dark;
+      overflow-y: scroll;
+      &::-webkit-scrollbar {
+        width: 3px;
       }
-
-      .notes {
-         height: 100%;
-         width: 100%;
-         border-top: 1px solid $dark;
-         overflow-y: scroll;
-         &::-webkit-scrollbar { width: 3px; }
-         &::-webkit-scrollbar-track { background: transparent; }
-         &::-webkit-scrollbar-thumb { background: #888; }
+      &::-webkit-scrollbar-track {
+        background: transparent;
       }
-
-      @media (max-width: 750px) {
-         width: 100%;
-         position: absolute;
-         left: -100%;
-         transition: .15s left ease-in-out;
-         &.navOpen { left: 0; }
+      &::-webkit-scrollbar-thumb {
+        background: #888;
       }
-   }
+    }
 
-   .window {
-      height: 100%;
-      width: calc(100% - 240px);
-
-      @media (max-width: 750px) {
-         width: 100%;
+    @media (max-width: 750px) {
+      width: 100%;
+      position: absolute;
+      left: -100%;
+      transition: 0.15s left ease-in-out;
+      &.navOpen {
+        left: 0;
       }
-   }
+    }
+  }
+
+  .window {
+    height: 100%;
+    padding-bottom: 2rem;
+    width: calc(100% - 240px);
+
+    @media (max-width: 750px) {
+      width: 100%;
+    }
+  }
 }
-
 </style>
--- src/views/NoteEdit.vue
+++ src/views/NoteEdit.vue
@@ -1,71 +1,148 @@
 <script>
+import marked from "marked";
+import "../markdown.scss";
+
 export default {
-  name: 'note-edit',
+  name: "note-edit",
 
   props: {
-     note: { type: Object, default: () => ({}) },
+    note: { type: Object, default: () => ({}) },
+  },
+
+  data: () => ({
+    previewMode: "plaintext",
+  }),
+
+  methods: {
+    changePreviewMode(mode) {
+      this.previewMode = mode;
+    },
+
+    getMarkdownPreview() {
+      return marked(this.note.content);
+    },
   },
 };
 </script>
 
 <template>
   <div class="note-edit">
-     <div class="title">
-        <input class="name" @input="e => $emit('name', e)" :value="note.name" />
-        <div class="date">{{ note.creationDate }}</div>
-     </div>
+    <div class="title">
+      <input class="name" @input="e => $emit('name', e)" :value="note.name" />
+      <div class="date">{{ note.creationDate }}</div>
+    </div>
 
-      <textarea
-         :value="note.content"
-         @input="e => $emit('content', e)"
-         placeholder="Note"
-      />
+    <div class="controls">
+      <button
+        @click="changePreviewMode('plaintext')"
+        :class="{ active: previewMode === 'plaintext' }"
+        class="control-btn"
+      >Plaintext</button>
+      <button
+        @click="changePreviewMode('markdown')"
+        :class="{ active: previewMode === 'markdown' }"
+        class="control-btn"
+      >Markdown</button>
+    </div>
+
+    <textarea
+      v-if="previewMode === 'plaintext'"
+      @input="e => $emit('content', e)"
+      :value="note.content"
+      placeholder="Note"
+    />
+
+    <div
+      :class="{ 'markdown-body': true }"
+      v-if="previewMode === 'markdown'"
+      v-html="getMarkdownPreview()"
+    ></div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+$header-height: 60px;
+$controls-height: 31px;
+
 .note-edit {
-   height: 100%;
-   width: 100%;
-   padding: 10px;
+  height: 100%;
+  width: 100%;
+  padding: 10px;
 
-   .title {
-      height: 60px;
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
+  .title {
+    height: 60px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
 
-      .name {
-         width: 50%;
-         background: transparent;
-         border: none;
-         outline: none;
-         border-bottom: 1px solid #323231;
-         margin-bottom: 6px;
-         font-size: 26px;
-         color: #f5f5f5;
-
-         @media (max-width: 750px) {
-            width: 100%;
-         }
-      }
-
-      .date {
-         height: 16px;
-         font-size: 12px;
-         font-style: italic;
-         color: #888;
-      }
-   }
-
-   textarea {
-      height: calc(100% - 60px);
-      width: 100%;
-      background: #212121;
+    .name {
+      width: 50%;
+      background: transparent;
       border: none;
       outline: none;
+      border-bottom: 1px solid #323231;
+      margin-bottom: 6px;
+      font-size: 26px;
       color: #f5f5f5;
-   }
+
+      @media (max-width: 750px) {
+        width: 100%;
+      }
+    }
+
+    .date {
+      height: 16px;
+      font-size: 12px;
+      font-style: italic;
+      color: #888;
+    }
+  }
+
+  textarea {
+    height: calc(100% - #{$header-height} - #{$controls-height});
+    width: 100%;
+    background: #212121;
+    border: none;
+    outline: none;
+    color: #f5f5f5;
+  }
+
+  .controls {
+    display: flex;
+    justify-content: center;
+    margin: 0.5rem 0;
+
+    @media (min-width: 750px) {
+      justify-content: flex-end;
+    }
+
+    * + * {
+      margin-left: 1rem;
+    }
+  }
+
+  .control-btn {
+    padding: 0.5rem 2rem;
+    border: 1px solid #fff;
+    color: #fff;
+    border-radius: 10px;
+    background: none;
+    cursor: pointer;
+    transition: 0s background ease-in-out;
+
+    &.active,
+    &:hover {
+      background: #fff;
+      color: #212121;
+    }
+  }
+
+  .markdown-body {
+    height: calc(100% - #{$header-height} - #{$controls-height});
+    padding: 0 1rem;
+    text-align: left;
+    overflow: auto;
+  }
 }
 </style>
