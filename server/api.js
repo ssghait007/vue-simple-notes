13ca19fce2a46fbfac98d7d5ed84a51539a96226
ssghait007 <ssghait.007@gmail.com>
2020-10-10 14:35:13 +0530

    todo comments

--- server/api.js
+++ server/api.js
@@ -18,34 +18,52 @@ const notes = [{
 }]
 
 router.get("/note", (req, res) => {
+    /*
+    * Add database get all notes query 
+    */
     res.json({
         Notes: notes
     });
 });
 
 router.get("/note/:id", (req, res) => {
+    /*
+    * Add database get single note query 
+    */
     const note = notes.filter(item => item.id == req.params.id)
     note.length ? res.json(note) : res.json([])
 });
 
 router.post("/note", (req, res) => {
+    /*
+    * Add database add note all query 
+    */
     res.json({
         msg: "success",
         desc: "Note added"
     });
 });
+
 router.put('/note/:id', (req, res) => {
+    /*
+    * Add database update note query 
+    */
     res.json({
         msg: "success",
         desc: "Note updated"
     });
 });
+
 router.delete('/note/:id', (req, res) => {
+    /*
+    * Add database delete note query 
+    */
     res.json({
         msg: "success",
         desc: "Note deleted"
     });
 });
+
 app.use(`/.netlify/functions/api`, router);
 
 module.exports = app;
